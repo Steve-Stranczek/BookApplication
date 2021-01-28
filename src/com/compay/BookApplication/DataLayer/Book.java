920a47455f79586d3b3820365b2aac1c3a2dbf85
Steve <stevestranczek@gmail.com>
2021-01-28 10:03:16 -0700

    Default project state

---
+++ src/com/compay/BookApplication/DataLayer/Book.java
@@ -0,0 +1,9 @@
+package com.compay.BookApplication.DataLayer;
+
+public class Book {
+    public int bookId;
+    public String bookTitle;
+    public int genreId;
+    public int numPages;
+    public int authorId;
+}
