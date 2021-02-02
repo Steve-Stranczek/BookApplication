0438be5536b6e1e8ee970b4845c38e36e63f5f08
Steve <stevestranczek@gmail.com>
2021-02-02 10:15:24 -0700

    removed unecessary comments

--- src/test/java/com/steve/BookApi/BookApiApplicationTests.java
+++ src/test/java/com/steve/BookApi/BookApiApplicationTests.java
@@ -1,15 +1,11 @@
 package com.steve.BookApi;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.steve.BookApi.model.Author;
 import com.steve.BookApi.model.Book;
 import com.steve.BookApi.model.Genre;
-import com.steve.BookApi.repository.BookRepository;
 import com.steve.BookApi.service.BookService;
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.Assert;
 import org.junit.Rule;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +13,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import com.steve.BookApi.SqlConstants;
 
-import javax.xml.crypto.Data;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Supplier;
-
-import static com.steve.BookApi.SqlConstants.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -113,21 +99,6 @@ class BookApiApplicationTests {
 
     }
 
-   // @Test
-   // public void greetingShouldReturnDefaultMessage() {
-    //    String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
-
-   // }
+    
 
 }
-
-// TODO
-/**
- * https://www.testcontainers.org/quickstart/junit_5_quickstart/
- * https://www.testcontainers.org/modules/databases/jdbc/
- * https://programmerfriend.com/testcontainers-springboot-real-database
- * https://www.baeldung.com/spring-boot-testcontainers-integration-test
- * https://medium.com/javarevisited/integration-tests-with-spring-boot-testcontainers-liquibase-and-junit-5-13fb1ae70b40
- * https://mydeveloperplanet.com/2020/05/05/easy-integration-testing-with-testcontainers/
- * https://spring.io/guides/gs/testing-web/
- **/
